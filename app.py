08074e091ed15150c1772eae2b94a86dfbf9feb2
Evan Petzoldt <evan.petzoldt@protonmail.com>
2022-03-27 10:44:03 +0000

    testing changes to add event to support single events

--- app.py
+++ app.py
@@ -498,7 +498,51 @@ def handle_manage_schedule_option_button(ack, body, client, logger, context):
             }
             day_options.append(new_option)
 
+        
         blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Is this a recurring or single event?"
+			    }
+            },
+            {
+                "type": "actions",
+                "block_id": "recurring_select_block",
+                "elements": [
+                    {
+                        "type": "radio_buttons",
+                        "options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Recurring event / beatdown",
+                                    "emoji": True
+                                },
+                                "value": "recurring"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Single event / beatdown",
+                                    "emoji": True
+                                },
+                                "value": "single"
+                            },
+                        ],
+                        "action_id": "add_event_recurring_select_action",
+                        "initial_option": {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Recurring event / beatdown",
+                                "emoji": True
+                            },
+                            "value": "recurring"
+                        }
+                    }
+                ]
+            },
             {
                 "type": "input",
                 "block_id": "ao_display_name_select",
@@ -681,6 +725,220 @@ def handle_manage_schedule_option_button(ack, body, client, logger, context):
         except Exception as e:
             logger.error(f"Error publishing home tab: {e}")
             print(e)
+
+@app.action("add_event_recurring_select_action")
+def handle_add_event_recurring_select_action(ack, body, client, logger, context):
+    ack()
+    logger.info(body)
+    print(body)
+    user_id = context["user_id"]
+    team_id = context["team_id"]
+
+    # logging.info('add an event - switch recurring type')
+
+    # # list of AOs for dropdown
+    # try:
+    #     with my_connect(team_id) as mydb:
+    #         sql_ao_list = f"SELECT ao_display_name FROM {mydb.db}.aos WHERE qsignups_enabled = 1 ORDER BY REPLACE(ao_display_name, 'The ', '');"
+    #         ao_list = pd.read_sql(sql_ao_list, mydb.conn)
+    #         ao_list = ao_list['ao_display_name'].values.tolist()
+    # except Exception as e:
+    #     logger.error(f"Error pulling AO list: {e}")
+
+    # ao_options = []
+    # for option in ao_list:
+    #     new_option = {
+    #         "text": {
+    #             "type": "plain_text",
+    #             "text": option,
+    #             "emoji": True
+    #         },
+    #         "value": option
+    #     }
+    #     ao_options.append(new_option)
+
+    # day_list = [
+    #     'Monday',
+    #     'Tuesday',
+    #     'Wednesday',
+    #     'Thursday',
+    #     'Friday',
+    #     'Saturday',
+    #     'Sunday'
+    # ]
+    # day_options = []
+    # for option in day_list:
+    #     new_option = {
+    #         "text": {
+    #             "type": "plain_text",
+    #             "text": option,
+    #             "emoji": True
+    #         },
+    #         "value": option
+    #     }
+    #     day_options.append(new_option)
+
+    
+    # blocks = [
+    #     {
+    #         "type": "section",
+    #         "text": {
+    #             "type": "mrkdwn",
+    #             "text": "Is this a recurring or single event?"
+    #         }
+    #     },
+    #     {
+    #         "type": "actions",
+    #         "elements": [
+    #             {
+    #                 "type": "radio_buttons",
+    #                 "options": [
+    #                     {
+    #                         "text": {
+    #                             "type": "plain_text",
+    #                             "text": "Recurring event / beatdown",
+    #                             "emoji": True
+    #                         },
+    #                         "value": "recurring"
+    #                     },
+    #                     {
+    #                         "text": {
+    #                             "type": "plain_text",
+    #                             "text": "Single event / beatdown",
+    #                             "emoji": True
+    #                         },
+    #                         "value": "single"
+    #                     },
+    #                 ],
+    #                 "action_id": "add_event_recurring_select_action"
+    #             }
+    #         ]
+    #     },
+    #     {
+    #         "type": "input",
+    #         "block_id": "ao_display_name_select",
+    #         "element": {
+    #             "type": "static_select",
+    #             "placeholder": {
+    #                 "type": "plain_text",
+    #                 "text": "Select an AO",
+    #                 "emoji": True   
+    #             },
+    #             "options": ao_options,
+    #             "action_id": "ao_display_name_select_action"
+    #         },
+    #         "label": {
+    #             "type": "plain_text",
+    #             "text": "AO",
+    #             "emoji": True
+    #         }  
+    #     },
+    #     {
+    #         "type": "input",
+    #         "block_id": "event_day_of_week_select",
+    #         "element": {
+    #             "type": "static_select",
+    #             "placeholder": {
+    #                 "type": "plain_text",
+    #                 "text": "Select a day",
+    #                 "emoji": True   
+    #             },
+    #             "options": day_options,
+    #             "action_id": "event_day_of_week_select_action"
+    #         },
+    #         "label": {
+    #             "type": "plain_text",
+    #             "text": "Day of Week",
+    #             "emoji": True
+    #         }  
+    #     },
+    #     {
+    #         "type": "input",
+    #         "block_id": "event_time_select",
+    #         "element": {
+    #             "type": "timepicker",
+    #             "initial_time": "05:30",
+    #             "placeholder": {
+    #                 "type": "plain_text",
+    #                 "text": "Select time",
+    #                 "emoji": True
+    #             },
+    #             "action_id": "event_time_select"
+    #         },
+    #         "label": {
+    #             "type": "plain_text",
+    #             "text": "Beatdown Start",
+    #             "emoji": True
+    #         }
+    #     },
+    #     {
+    #         "type": "input",
+    #         "block_id": "add_event_datepicker",
+    #         "element": {
+    #             "type": "datepicker",
+    #             "initial_date": date.today().strftime('%Y-%m-%d'),
+    #             "placeholder": {
+    #                 "type": "plain_text",
+    #                 "text": "Select date",
+    #                 "emoji": True
+    #             },
+    #             "action_id": "add_event_datepicker"
+    #         },
+    #         "label": {
+    #             "type": "plain_text",
+    #             "text": "Start Date",
+    #             "emoji": True
+    #         }
+    #     },
+    #     {
+    #         "type": "actions",
+    #         "block_id": "submit_cancel_buttons",
+    #         "elements": [
+    #             {
+    #                 "type": "button",
+    #                 "text": {
+    #                     "type": "plain_text",
+    #                     "text": "Submit",
+    #                     "emoji": True
+    #                 },
+    #                 "value": "submit",
+    #                 "action_id": "submit_add_event_button",
+    #                 "style": "primary"
+    #             },
+    #             {
+    #                 "type": "button",
+    #                 "text": {
+    #                     "type": "plain_text",
+    #                     "text": "Cancel",
+    #                     "emoji": True
+    #                 },
+    #                 "value": "cancel",
+    #                 "action_id": "cancel_button_select",
+    #                 "style": "danger"
+    #             }
+    #         ]
+    #     },
+    #     {
+    #     "type": "context",
+    #     "elements": [
+    #             {
+    #                 "type": "mrkdwn",
+    #                 "text": "Please wait after hitting Submit, and do not hit it more than once"
+    #             }
+    #         ]
+    #     }
+    # ]
+    # try:
+    #     client.views_publish(
+    #         user_id=user_id,
+    #         view={
+    #             "type": "home",
+    #             "blocks": blocks
+    #         }
+    #     )
+    # except Exception as e:
+    #     logger.error(f"Error publishing home tab: {e}")
+    #     print(e)
 
 @app.action("edit_event_ao_select")
 def handle_edit_event_ao_select(ack, body, client, logger, context):
