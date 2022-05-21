9b67b29d267cecfc07df08cd9e8d49cb627da3a1
Evan Petzoldt <evan.petzoldt@protonmail.com>
2022-05-21 12:05:40 +0000

    added some debugging

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def get_user_names(array_of_user_ids, logger, client):
     return names
 
 def refresh_home_tab(client, user_id, logger, top_message, team_id, context):
-
+    print(context)
     upcoming_qs_df = pd.DataFrame()
     try:
         with my_connect(team_id) as mydb:
