3016516c1c0a6d56ce21635019e302855e126105
Paul Christmann <paul.christmann@rasa.io>
2022-12-17 13:07:39 -0600

    Fixed the delete

--- qsignups/slack/handlers/weekly.py
+++ qsignups/slack/handlers/weekly.py
@@ -7,9 +7,7 @@ from datetime import date
 
 def delete(client, user_id, team_id, logger, input_data) -> UpdateResponse:
 
-    event_id = input_data['id']
-
-    weekly_event = DbManager.get_record(vwWeeklyEvents, event_id)
+    weekly_event = DbManager.get_record(vwWeeklyEvents, input_data)
 
     # in the future we can use the FK from Weekly
     master_filter = [
@@ -23,8 +21,8 @@ def delete(client, user_id, team_id, logger, input_data) -> UpdateResponse:
     # Perform deletions
     try:
         DbManager.delete_records(Master, master_filter)
-        DbManager.delete_record(Weekly, event_id)
-        return UpdateResponse(success = True, message=f"I've deleted all future {weekly_event.event_type}s from the schedule for {weekly_event.event_day_of_week}s at {weekly_event.event_time} at {weekly_event.ao_display_name}.")
+        DbManager.delete_record(Weekly, weekly_event.id)
+        return UpdateResponse(success = True, message=f"I've deleted all future {weekly_event.ao_display_name}s from the schedule for {weekly_event.event_day_of_week}s at {weekly_event.event_time} at {weekly_event.ao_display_name}.")
     except Exception as e:
         logger.error(f"Error deleting: {e}")
         return UpdateResponse(success = False, message = f"Sorry, there was an error of some sort; please try again or contact your local administrator / Weasel Shaker. Errors:\n{e}")
