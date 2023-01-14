54f44c63524bf58afeaaa42bb10d3c9405bc56e9
Paul Christmann <paul.christmann@rasa.io>
2023-01-14 08:12:26 -0600

    Wrap all google access in try/catch

--- qsignups/google/authenticate.py
+++ qsignups/google/authenticate.py
@@ -3,10 +3,6 @@ import json, os
 from qsignups.database import DbManager
 from qsignups.database.orm import Region
 
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-
 # If this gets modified, then all authenticated data (pickle data) needs to get flushed
 SCOPES = [
   'https://www.googleapis.com/auth/calendar'
@@ -25,12 +21,20 @@ GOOGLE_CREDENTIALS = {
 def __load_region_credentials(team_id):
   region: Region = DbManager.get_record(Region, team_id)
   if region.google_auth_data:
-    return Credentials.from_authorized_user_info(region.google_auth_data)
+    try:
+
+      from google.oauth2.credentials import Credentials
+      return Credentials.from_authorized_user_info(region.google_auth_data)
+    except:
+      return None
   else:
     return None
 
 def __get_refreshed_credentials(team_id):
   try:
+
+    from google.auth.transport.requests import Request
+
     region_credentials = __load_region_credentials(team_id)
     if region_credentials and region_credentials.valid:
       return region_credentials
@@ -54,10 +58,15 @@ def connect(team_id):
   if region_credentials and region_credentials.valid:
     return region_credentials
   else:
-    flow = InstalledAppFlow.from_client_config(
-      client_config = GOOGLE_CREDENTIALS,
-      scopes = SCOPES)
-    region_credentials = flow.run_local_server(port=0)
+    try:
+
+      from google_auth_oauthlib.flow import InstalledAppFlow
+      flow = InstalledAppFlow.from_client_config(
+        client_config = GOOGLE_CREDENTIALS,
+        scopes = SCOPES)
+      region_credentials = flow.run_local_server(port=0)
+    except:
+      pass
 
   if region_credentials:
     DbManager.update_record(Region, team_id, {
